1adbf0dae2f29554159fcb589dffab1ccbc625e8
Jason <jsato03@gmail.com>
2018-03-08 15:25:15 -0800

    [feature] ability to remove added friends

--- client/src/components/Friends/Friend.jsx
+++ client/src/components/Friends/Friend.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
+import axios from "axios";
 
-const Friend = props => {
-  return (
-    <li>
-      {props.friend.username} ({props.friend.email}})
-    </li>
-  );
-};
+class Friend extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  async handleDeleteClick(clickedFriendId) {
+    const id = Number(localStorage.getItem("id"));
+    console.log(clickedFriendId, id);
+    console.log("clicked delete button");
+    const { data } = await axios.delete(
+      `http://localhost:3396/api/friends/deleteFriend/${id}/${clickedFriendId}`
+    );
+    console.log("received data: ", data);
+    // this.setState({ requested: true });
+  }
+
+  render() {
+    return (
+      <li>
+        {this.props.friend.username} ({this.props.friend.email}})
+        <input
+          type="button"
+          value="Remove friend"
+          onClick={() => this.handleDeleteClick(this.props.friend.id)}
+          disabled={this.state.requested}
+        />
+      </li>
+    );
+  }
+}
 
 export default Friend;
