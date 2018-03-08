922d76cca7f82fd76c35585d67b057d3a048a25f
Jason <jsato03@gmail.com>
2018-03-08 11:14:24 -0800

    Added friends page

---
+++ client/src/components/Friends/Friends.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class Friends extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h1>aaa page</h1>;
+  }
+}
