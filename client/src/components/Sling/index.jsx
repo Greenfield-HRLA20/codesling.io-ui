3a69815dc96aa08dbbf7162328f0bcc9512fa158
Jason <jsato03@gmail.com>
2018-03-08 10:31:15 -0800

    Merge conflicts

--- client/src/components/Sling/index.jsx
+++ client/src/components/Sling/index.jsx
@@ -22,29 +22,17 @@ class SlingIndex extends Component {
     if (this.props.location.state) {
       return (
         <Sling
-<<<<<<< HEAD
-          socket={this.state.socket}
-          challenge={this.props.location.state.challenge}
-          history={this.props.history}
-=======
           history={this.props.history}
           socket={this.state.socket}
           challenge={this.props.location.state.challenge}
->>>>>>> [fix] add navigation to Duel page
         />
       );
     } else {
       return (
         <Sling
-<<<<<<< HEAD
-          socket={this.state.socket}
-          challenge={{}}
-          history={this.props.history}
-=======
           history={this.props.history}
           socket={this.state.socket}
           challenge={{}}
->>>>>>> [fix] add navigation to Duel page
         />
       );
     }
