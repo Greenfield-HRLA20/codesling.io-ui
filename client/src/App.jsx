78e24a096b05df325e19002209817bf8603f8dd9
Jason <jsato03@gmail.com>
2018-03-08 15:13:41 -0800

    [feature] ability to see current friends and add new friends

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,8 @@ import Challenge from "./components/Challenge/index.jsx";
 import AddChallenge from "./components/Challenge/AddChallenge/index.jsx";
 import Protected from "./components/globals/Protected";
 import History from "./components/History/index.jsx";
-import Friends from "./components/Friends/Friends.jsx";
+import Friends from "./components/Friends/index.jsx";
+import AllUsers from "./components/AllUsers/index.jsx";
 
 class App extends Component {
   constructor(props) {
@@ -54,6 +55,7 @@ class App extends Component {
           {/* <Route path="/slinger" component={Slinger} /> */}
           <Route path="/history" component={History} />
           <Route path="/friends" component={Friends} />
+          <Route path="/allUsers" component={AllUsers} />
           <Route path="/:sling" component={Sling} />
           <Route path="/" component={LandingPage} />
         </Switch>
